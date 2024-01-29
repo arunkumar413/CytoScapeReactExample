09e947b1bb6d08399964533fec6dcb2a1ca66576
arun <arunkumar413@gmail.com>
2024-01-29 23:45:13 +0530

    adding dagre layout

--- src/CytoScape.jsx
+++ src/CytoScape.jsx
@@ -1,23 +1,40 @@
 import React, { useEffect, useRef } from "react";
 import cytoscape from "cytoscape";
+import dagre from 'cytoscape-dagre';
 
 export const MyCytoscapeComponent = () => {
   const cyContainerRef = useRef(null);
+
+  cytoscape.use( dagre );
 
   useEffect(() => {
     const cy = cytoscape({
       container: cyContainerRef.current,
       elements: {
         nodes: [
-          { data: { id: "a", label: "Node A" } },
-          { data: { id: "b", label: "Node B" } },
-          { data: { id: "c", label: "Node C" } },
-          { data: { id: "d", label: "Node D" } },
+          { data: { id: "Fruits", label: "Fruits" } },
+          { data: { id: "Apple", label: "Apple" } },
+          { data: { id: "Mango", label: "Mango" } },
+          { data: { id: "Banana", label: "Banana" } },
+          { data: { id: "Vegetables", label: "Vegetables" } },
+          { data: { id: "Tomato", label: "Tomato" } },
+          { data: { id: "Potato", label: "Potato" } },
+          { data: { id: "Cauli flower", label: "Cauli flower" } },
+
+
+
+
         ],
         edges: [
-          { data: { source: "a", target: "b", label: "A to B" } },
-          { data: { source: "b", target: "c", label: "B to C" } },
-          { data: { source: "a", target: "d", label: "A to D" } },
+          { data: { source: "Fruits", target: "Apple", label: "" } },
+          { data: { source: "Fruits", target: "Mango", label: "" } },
+          { data: { source: "Fruits", target: "Banana", label: "" } },
+          { data: { source: "Vegetables", target: "Tomato", label: "" } },
+          { data: { source: "Vegetables", target: "Potato", label: "" } },
+          { data: { source: "Vegetables", target: "Cauli flower", label: "" } },
+
+
+
         ],
       },
       style: [
@@ -25,7 +42,13 @@ export const MyCytoscapeComponent = () => {
           selector: "node",
           style: {
             label: "data(label)",
-            backgroundColor: "teal",
+            backgroundColor: "grey",
+            shape: "round-rectangle",
+            width: 100,
+            height: 100,
+            "text-halign": "center",
+            "text-valign": "center",
+            color: "white",
           },
         },
         {
@@ -36,8 +59,27 @@ export const MyCytoscapeComponent = () => {
             "target-arrow-shape": "triangle",
           },
         },
+
+        {
+          selector: 'node[id = "Vegetables"]',
+          style:{
+          backgroundColor:'green'
+          }
+        },
+        {
+          selector: 'node[id = "Fruits"]',
+          style:{
+          backgroundColor:'blue'
+          }
+        }
       ],
-      layout: { name: "circle", rows: 2, columns: 2 },
+      layout: {
+        name: "dagre",
+        fit: true,
+        avoidOverlap: true,
+        // rows: 10,
+        // cols: 10,
+      },
     });
 
     // Cleanup function
